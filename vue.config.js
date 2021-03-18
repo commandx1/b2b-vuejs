453dabd0406bbcd88a36e9f68560cb4ce93b4d22
commandx1 <serhatbelen7@gmail.com>
2021-03-18 23:29:58 +0300

    build fix

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,9 @@ module.exports = {
     proxy: {
       "/api": {
         target: "https://desolate-reaches-37168.herokuapp.com/",
+        // target: "http://localhost:5000/",
       },
     },
   },
-  outputDir: path.resolve(__dirname, "../backend/public"),
+  // outputDir: path.resolve(__dirname, "../backend/public"),
 };
